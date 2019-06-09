29b01660122919a50ddb909c6630ea3b15cbd588
Robin Barkas <barkasrobin@gmail.com>
2019-06-09 15:59:49 +0200

    Désactivation warnings depreciation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fabric import Connection
 
+import warnings
+import cryptography
+warnings.simplefilter("ignore", cryptography.utils.CryptographyDeprecationWarning)
+
 pc1 = Connection('linuxlocal@192.168.1.156')
 
 result = pc1.run('hostname -s', hide=True)
