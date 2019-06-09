c5cd2b4a56c33d66d7d908f10ff1d57b41f7a8c3
Robin Barkas <barkasrobin@gmail.com>
2019-06-09 16:19:22 +0200

    Création classe Ordinateur

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fabric import Connection
 
+# Cache les avertissements de dépréciation envoyés par les dépendances de la librairie Fabric
 import warnings
 import cryptography
 warnings.simplefilter("ignore", cryptography.utils.CryptographyDeprecationWarning)
+
+# Définit un ordinateur distant sur lequel on peut se connecter
+class Ordinateur:
+    def __init__(self):
+        self.name = "VM LinuxLocal"
+        self.ip   = "192.168.1.156"
+        self.user = "linuxlocal"
+    
+    # Renvoie adresse ssh (user@ip)
+    def _get_ssh_address(self):
+        return self.user + "@" + self.ip
 
 pc1 = Connection('linuxlocal@192.168.1.156')
 
@@ -10,3 +22,5 @@ result = pc1.run('hostname -s', hide=True)
 print("Résultat : " + str(result.stdout))
 print("Code retour : " + str(result.return_code))
 print("Code erreur : " + str(result.stderr))
+
+pc1.close()
