4f2c1a846c1a50e1a6f55c0d43097db64032bee0
Robin Barkas <barkasrobin@gmail.com>
2019-06-09 16:03:29 +0200

    Affichage infos action effectuée

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ warnings.simplefilter("ignore", cryptography.utils.CryptographyDeprecationWarnin
 pc1 = Connection('linuxlocal@192.168.1.156')
 
 result = pc1.run('hostname -s', hide=True)
-print(result.stdout)
+print("Résultat : " + str(result.stdout))
+print("Code retour : " + str(result.return_code))
+print("Code erreur : " + str(result.stderr))
