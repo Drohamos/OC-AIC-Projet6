805d155291f577386ab6ac3b3eb1219b9fee5055
Robin Barkas <barkasrobin@gmail.com>
2019-06-11 11:21:08 +0200

    Encapsulation attribut "ip" + ajout validation

--- main.py
+++ main.py
@@ -37,6 +37,23 @@ class Ordinateur:
     def ssh_address(self):
         return self.user + "@" + self.ip
 
+    @property
+    def ip(self):
+        return self.__ip
+
+    @ip.setter
+    def ip(self, ip):
+        if not (isinstance(ip, str)):
+            raise Exception("ip doit être une chaîne de caractères")
+
+        # Suppression des espaces inutiles
+        ip = ip.strip()
+
+        if not (ip):
+            raise Exception("ip ne peut pas être vide")
+
+        self.__ip = ip
+
 def test():
     book = OrdinateurBookmarker()
 
