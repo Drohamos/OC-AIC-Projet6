0e6dc622316b8b69fd85d560ff8c0d440a243fcf
Robin Barkas <barkasrobin@gmail.com>
2019-06-19 14:20:57 +0200

    Création classe FormScenario

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 # AICToolbox
 # Auteur : Robin BARKAS
 
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QGridLayout, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QMessageBox, QGroupBox
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QGridLayout, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QMessageBox, QGroupBox, QFrame
 
 import models
 import services
@@ -162,6 +162,24 @@ class FormOrdinateur(QHBoxLayout):
         self.edit_ip.setText("")
         self.edit_user.setText("")
         self.edit_name.setText("")
+
+class FormScenario(QFrame):
+    def __init__(self, fields):
+        super().__init__()
+        self.setUI(fields)
+
+    def setUI(self, fields):
+        self.hbox = QHBoxLayout()
+        self.setLayout(self.hbox)
+
+        for field in fields:
+            self.hbox.addWidget(utils.LineEditWithPlaceholder("Interface (ex : ens33)"))
+    
+    def add_line_edit (self, placeholder = None):
+        line_edit = QLineEdit()
+
+        if not (placeholder is None):
+            line_edit.setPlaceholderText(placeholder)
 
 class ResultConsole(QVBoxLayout):
     def __init__(self):
